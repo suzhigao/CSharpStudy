0f15c68dde78ecf0a61c057f1b50ecf2ff6e5503
Lewis Su <1763489853@qq.com>
2024-11-19 22:21:18 +0800

    Add project files.

---
+++ FirstWithCSharp/FirstWithCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWithCSharp //命名空间：用于组织代码，避免类名冲突
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World!\n Welcome to the home of CSharp!");
+
+            Console.ReadKey();
+        }
+    }
+}

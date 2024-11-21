c68a446edb08f8da8b7f6c010bfef8272ff617c7
Lewis Su <1763489853@qq.com>
2024-11-21 21:11:17 +0800

    Add WPF Learning.

---
+++ WpfHelloWorld/WpfHelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, RoutedEventArgs e)
+        {
+            tbShowHello.Text = "Hello, World";
+        }
+
+        private void tbShowHello_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
